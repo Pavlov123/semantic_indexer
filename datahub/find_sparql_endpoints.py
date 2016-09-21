767e880401b9e7e26cc74c49f67fa881d6977b01
Paul Poulakis <poulakis.paul@gmail.com>
2016-09-21 20:43:54 +0300

    Update find sparql endpoints.

--- datahub/find_sparql_endpoints.py
+++ datahub/find_sparql_endpoints.py
@@ -9,19 +9,9 @@ client = RemoteCKAN(settings['ckan_host'], settings['api_key'])
 package_ids = client.action.package_list()
 
 sparql = []
-for package_id in package_ids:
-    package = client.action.package_show(id=package_id)
-
-    resources = package.get('resources')
-    sparql_endpoints = [
-        resource['url'] for resource in resources
-        if resource['format'] == 'api/sparql' or resource['description'] == 'SPARQL endpoint'
-    ]
-    if sparql_endpoints:
-            sparql.append((file, ', '.join(sparql_endpoints)))
-            continue
-
 with open('results_sparql.txt', 'w') as list_file:
-    for package, urls in sparql:
-        line = u'%s: %s\n' % (package, urls)
-        list_file.write(line.encode('utf-8'))
+    for package_id in package_ids:
+        package = client.action.package_show(id=package_id)
+        for resource in package.get('resources'):
+            if resource['format'] == 'api/sparql' or resource['description'] == 'SPARQL endpoint':
+                list_file.write(resource['url'].encode('utf-8'))
